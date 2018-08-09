72a4713efcb90c6739dcb1e30a04fa504e501776
Kyle Meiklejohn <kmeiklejohn4@gmail.com>
2018-08-09 14:47:37 -0400

    part2

--- src/App.js
+++ src/App.js
@@ -6,61 +6,92 @@ class TodoItem extends React.Component{
 
   render(){
     return (
-      <li className={this.props.value}>
+      <li className={this.props.completed?'completed':''}>
         <div className='view'>
-          <input className='toggle' type='checkbox' checked={this.props.completed}/>
+          <input onClick={this.props.onClickCheck} id={this.props.id} className='toggle' type='checkbox' defaultChecked={this.props.completed}/>
           <label>{this.props.title}</label>
-          <button className='destroy'/>
+          <button onClick={this.props.onClickDestroy} className='destroy'/>
         </div>
       </li>
     )
   }
 }
-
-
 class TodoList extends React.Component{
-  constructor(props){
-    super(props)
-    this.state = {todo:todoList}  
+  state = {todos:todoList}  
+  
+ handleInputChange = (event) => {
+   if(event.key==='Enter'){
+  const newTodoList = this.state.todos.slice();
+   newTodoList.push({
+        title:event.target.value,
+        completed : false})
+       this.setState({todos:newTodoList})
   }
-  render(){
-    const todos = this.state.todo
-    return(
+ }
+isChecked = (id) => () => {
+  const newTodoList = this.state.todos.slice()
+  
+  newTodoList.forEach(todo => {
+      if(id === todo.id){
+        todo.completed = !todo.completed
+        }          
+      });
+      this.setState({todos:newTodoList})
+  
+}
+  destroyItem = (id) => () =>{
+    const newTodoList= this.state.todos.slice()
+        newTodoList.forEach((todo, index) =>{
+    if(todo.id === id){
+      newTodoList.splice(index, 1)
+    }
+  })
+  this.setState({todos: newTodoList})
+  }
 
-    <body>
+  destroyList = () => {
+      const completedTodos = this.state.todos.filter(todo => !todo.completed)
+      this.setState({todos: completedTodos})
+  }
+
+  render(){
+    const todos = this.state.todos
+    return(
+    <React.Fragment>
+      
       <header className='header'>
           <h1>todos</h1>
-          <input className='new-todo' placeholder='What needs to be done?' autoFocus/>>
+          <input className='new-todo' placeholder='What needs to be done?' autoFocus completed={this.isChecked} onKeyDown={this.handleInputChange}/>
       </header>
-
         <section className='main'>
             <ul className='todo-list'>
                {todos.map( todo =>(
                <TodoItem 
-               title={todo.title}
-               completed={todo.completed}/>))}
+                 title={todo.title}
+                 key={todo.id}
+                 onClickDestroy={this.destroyItem(todo.id)}
+                 completed={todo.completed}
+                 onClickCheck={this.isChecked(todo.id)}/>))}
             </ul>
         </section>
-        <footer className='footer'>
-        <span className='todo-count'><strong>0</strong> item(s) left</span>
-        <button className='clear-complete'>Clear completed</button>
-
-        </footer>
-    </body>
+        
+         <div className='footer'>
+          <span className='todo-count'><strong>0</strong> item(s) left</span>
+          <button onClick={this.destroyList} className='clear-complete'>Clear completed</button>
+        </div>
+    </React.Fragment>
     )
     }
 }
-
 class App extends Component {
   render() {
     return (
-      <body>
+      <React.Fragment>
         <section className="todoapp">
           <TodoList/>
         </section>
-      </body>
+      </React.Fragment>
     );
   }
 }
-
 export default App;
